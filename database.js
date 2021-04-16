46b325fec2d908d6d0f4518bee6204b17f2f3505
PaloAlto-TI <tecnologiasdelainformacion@paloalto.com.ec>
2021-04-16 16:48:40 -0500

    Conexion a MongoDB Atlas

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost:27017/distribucion-inventario';
+const URI = 'mongodb://palo:palo123@cluster0-shard-00-00.sfqam.mongodb.net:27017,cluster0-shard-00-01.sfqam.mongodb.net:27017,cluster0-shard-00-02.sfqam.mongodb.net:27017/test?ssl=true&replicaSet=atlas-6cdhgc-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 mongoose.connect(URI , { useUnifiedTopology: true, useNewUrlParser: true })
     .then(db => console.log('Conectamos a base'))
