9cdd7ed0402fef0fcba763376becd8431da6e07c
PaloAlto-TI <tecnologiasdelainformacion@paloalto.com.ec>
2021-04-26 11:57:07 -0500

    Fix Deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const { mongoose } = require("./database");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
 app.use(cors());
